d23b1fbb186adebb31016e9d21b42df3c5959681
TXort <46675337+TXort@users.noreply.github.com>
2024-05-21 20:35:48 +0200

    Add config

---
+++ src/app/app.config.ts
@@ -0,0 +1,20 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {AuthInterceptor} from "./interceptors/auth.interceptor";
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide:HTTP_INTERCEPTORS,
+      useClass:AuthInterceptor,
+      multi:true
+    },
+    provideAnimationsAsync()
+  ],
+};
