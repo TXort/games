48ca4255fbb244621478852d84d15b28af9ebcf7
TXort <46675337+TXort@users.noreply.github.com>
2024-05-25 01:07:45 +0200

    Add game card component and styles

---
+++ src/app/component/game-card/game-card.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input} from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {MatCard} from "@angular/material/card";
+
+@Component({
+  selector: 'app-game-card',
+  standalone: true,
+  imports: [
+    MatIcon,
+    MatCard
+  ],
+  templateUrl: './game-card.component.html',
+  styleUrl: './game-card.component.scss'
+})
+
+
+export class GameCardComponent {
+
+  @Input() title: string | undefined;
+  @Input() description: string | undefined;
+  @Input() game_picture_url: string | undefined;
+  @Input() esrb: string | undefined;
+  @Input() license: string | undefined;
+  @Input() avg_rating: number | undefined;
+
+}
