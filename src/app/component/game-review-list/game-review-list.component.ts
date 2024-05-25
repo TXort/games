8af004162cb80faf6b70048e746ee82cab4a93d3
TXort <46675337+TXort@users.noreply.github.com>
2024-05-25 17:28:07 +0200

    Add game review list component

---
+++ src/app/component/game-review-list/game-review-list.component.ts
@@ -0,0 +1,34 @@
+import {Component, Input} from '@angular/core';
+import {Review} from "../../models/review.model";
+import {ReviewService} from "../../service/review.service";
+import {MatCard} from "@angular/material/card";
+import {MatIcon} from "@angular/material/icon";
+import {MatMenu, MatMenuTrigger} from "@angular/material/menu";
+
+@Component({
+  selector: 'app-game-review-list',
+  standalone: true,
+  imports: [
+    MatCard,
+    MatIcon,
+    MatMenu,
+    MatMenuTrigger
+  ],
+  templateUrl: './game-review-list.component.html',
+  styleUrl: './game-review-list.component.scss'
+})
+export class GameReviewListComponent {
+
+  @Input() reviews: Array<Review> | undefined;
+
+  constructor(private reviewService: ReviewService) {}
+
+  public canDelete(review: Review): boolean {
+    return true;
+  }
+
+  public onDelete(review: Review): void {
+    return;
+  }
+
+}
