5aaa9d7bdfb0bead29890ee2767f227921cebd47
TXort <46675337+TXort@users.noreply.github.com>
2024-05-26 01:16:56 +0200

    Add new component

--- src/app/pages/admin-panel/admin-panel.component.ts
+++ src/app/pages/admin-panel/admin-panel.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {VideogameFormComponent} from "../../component/videogame-form/videogame-form.component";
 
 @Component({
   selector: 'app-admin-panel',
   standalone: true,
-  imports: [],
+  imports: [
+    VideogameFormComponent
+  ],
   templateUrl: './admin-panel.component.html',
   styleUrl: './admin-panel.component.css'
 })
