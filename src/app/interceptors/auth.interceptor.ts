9ccae2475b4eccc2651d4188a3f7349ad734084a
TXort <46675337+TXort@users.noreply.github.com>
2024-05-26 01:18:14 +0200

    Handle non-signed in user

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -2,18 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {environment} from "../../environments/environment.development";
+import {StorageService} from "../service/storage.service";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
+  constructor(private ls: StorageService) { }
+
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
-    request = request.clone({
-      setHeaders: {
-         apikey: environment.supabase.key,
-         Authorization: `Bearer ${environment.supabase.key}`
+    if (this.ls.get('sb-jzfegpsbaxtsanqvzcid-auth-token')) {
+
+      let access_token: string | null = this.ls?.get('sb-jzfegpsbaxtsanqvzcid-auth-token').access_token;
+
+      request = request.clone({
+        setHeaders: {
+          apikey: environment.supabase.key,
+          Authorization: `Bearer ${access_token}`
         }
-    });
+      });
+    } else
+      request = request.clone({
+        setHeaders: {
+          apikey: environment.supabase.key
+        }
+      });
 
     return next.handle(request);
   }
