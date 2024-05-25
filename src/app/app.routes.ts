d6c8624f1c199f31290aaf8e5b6ae221c24533b8
TXort <46675337+TXort@users.noreply.github.com>
2024-05-25 20:00:59 +0200

    Add admin panel route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,6 @@ import {GameReviewsComponent} from "./pages/game-reviews/game-reviews.component"
 
 export const routes: Routes = [
   {
-    path: '',
-    loadComponent: () =>
-      import('./pages/games/games.component').then(
-        (com) => com.GamesComponent
-      ),
-  },
-  {
     path: 'games',
     children: [
       {
@@ -28,6 +21,20 @@ export const routes: Routes = [
           ),
       }
     ]
+  },
+  {
+    path: 'admin-panel',
+    loadComponent: () =>
+      import('./pages/admin-panel/admin-panel.component').then(
+        (com) => com.AdminPanelComponent
+      ),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/games/games.component').then(
+        (com) => com.GamesComponent
+      ),
   },
   {
     path: '**',
