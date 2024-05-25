b66ca85cce9514fc77501a6e391b82fcb51f4bdb
TXort <46675337+TXort@users.noreply.github.com>
2024-05-25 17:27:26 +0200

    Add comment validation

---
+++ src/app/validators/comment.validator.ts
@@ -0,0 +1,11 @@
+import {FormControl, FormGroup, ValidationErrors} from "@angular/forms";
+
+export function commentValidator(control: FormControl) : ValidationErrors | null {
+
+  const badWords = ['infsus', 'infsys', 'infsyss'];
+
+  if (badWords.some(badWord => control.value.includes(badWord))) {
+    return { comment: 'bad word detected' };
+  }
+  return null;
+}
