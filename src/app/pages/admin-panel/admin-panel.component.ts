f0e78ac7234a29a43903f7dc76b8ce093bcdaf65
TXort <46675337+TXort@users.noreply.github.com>
2024-05-26 21:42:31 +0200

    Add admin panel page submission logic

--- src/app/pages/admin-panel/admin-panel.component.ts
+++ src/app/pages/admin-panel/admin-panel.component.ts
@@ -1,15 +1,58 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {VideogameFormComponent} from "../../component/videogame-form/videogame-form.component";
+import {VideogameService} from "../../service/videogame.service";
+import {CompanyService} from "../../service/company.service";
+import {Company} from "../../models/company.model";
+import {EsrbRating} from "../../models/esrbrating.model";
+import {License} from "../../models/license.model";
+import {combineLatest, map, Observable} from "rxjs";
+import {AsyncPipe} from "@angular/common";
+import {VideoGameForm} from "../../models/videogameform.model";
+import {IRawVideoGameSubmission} from "../../interfaces/rawVideoGame.interface";
 
 @Component({
   selector: 'app-admin-panel',
   standalone: true,
   imports: [
-    VideogameFormComponent
+    VideogameFormComponent,
+    AsyncPipe
   ],
   templateUrl: './admin-panel.component.html',
   styleUrl: './admin-panel.component.css'
 })
 export class AdminPanelComponent {
+
+  formDataInput$: Observable<VideoGameForm>;
+
+
+  constructor(
+    private gameService: VideogameService,
+    private companyService: CompanyService
+  ) {
+    this.formDataInput$ = combineLatest([
+      this.companyService.getPublishers(),
+      this.companyService.getDevelopers(),
+      this.gameService.getLicenses(),
+      this.gameService.getEsrbRatings()
+    ]).pipe(
+      map(([publishers, developers, licenses, esrbs]) => {
+        return new VideoGameForm({
+          publishers: publishers,
+          developers: developers,
+          licences: licenses,
+          esrbs: esrbs
+        });
+      })
+    );
+  }
+
+  public onSubmit(gameData: IRawVideoGameSubmission): void { // FIX
+    this.gameService.submitVideoGame(gameData).subscribe(() => {
+      console.log('Submitted');
+    })
+  }
+
+
+
 
 }
