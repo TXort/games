941bfe3a133467e16ed1a63e7737a453629d498f
TXort <46675337+TXort@users.noreply.github.com>
2024-05-26 22:14:16 +0200

    Add env variables

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  appname: 'Game',
+  supabase: {
+    url: 'https://jzfegpsbaxtsanqvzcid.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6ZmVncHNiYXh0c2FucXZ6Y2lkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYxNDY2NTksImV4cCI6MjAzMTcyMjY1OX0.ZvGjPXqzhe4aX8yn_nH3hsImlz0_JOzZx0tll7W7N6I',
+  },
+};
