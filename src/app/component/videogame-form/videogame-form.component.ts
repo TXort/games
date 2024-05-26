7fde9936a165961836a5c1bea9ef1dd208f43a3f
TXort <46675337+TXort@users.noreply.github.com>
2024-05-26 21:42:44 +0200

    Add videogame form logic

--- src/app/component/videogame-form/videogame-form.component.ts
+++ src/app/component/videogame-form/videogame-form.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {MatOption} from "@angular/material/autocomplete";
-import {MatFormField, MatSelect} from "@angular/material/select";
-import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {MatError, MatFormField, MatSelect} from "@angular/material/select";
+import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatCard, MatCardContent, MatCardTitle} from "@angular/material/card";
 import {MatButton} from "@angular/material/button";
 import {MatInput} from "@angular/material/input";
+import {Company} from "../../models/company.model";
+import {EsrbRating} from "../../models/esrbrating.model";
+import {License} from "../../models/license.model";
+import {IRawReviewSubmission} from "../../interfaces/rawReview.interface";
+import {VideoGameForm} from "../../models/videogameform.model";
+import {IRawVideoGameSubmission} from "../../interfaces/rawVideoGame.interface";
 
 @Component({
   selector: 'app-videogame-form',
@@ -18,37 +24,67 @@ import {MatInput} from "@angular/material/input";
     MatCardTitle,
     MatCardContent,
     MatButton,
-    MatInput
+    MatInput,
+    MatError
   ],
   templateUrl: './videogame-form.component.html',
   styleUrl: './videogame-form.component.scss'
 })
 export class VideogameFormComponent {
-  publishers: Array<any> = [];
-  developers: Array<any> = [];
-  esrbs: Array<any> = [];
-  platforms: Array<any> = [];
-  licenses: Array<any> = [];
-  form: FormGroup = new FormGroup({});
 
-  constructor() {
-    this.form = new FormGroup({
-      title: new FormControl(''),
-      imageUrl: new FormControl(''),
-      description: new FormControl(''),
-      publisher: new FormControl(''),
-      developer: new FormControl(''),
-      esrb: new FormControl(''),
-      platform: new FormControl(''),
-      license: new FormControl(''),
-      cover: new FormControl(''),
-      trailer: new FormControl(''),
-      website: new FormControl(''),
-    });
+  @Input() publishers: Array<Company> = [];
+  @Input() developers: Array<Company> = [];
+  @Input() esrbs: Array<EsrbRating> = [];
+  @Input() licenses: Array<License> = [];
+
+
+  reviewFormGroup: FormGroup = this.fb.group({
+      title: ['', [Validators.required]],
+      imageUrl: ['', [Validators.required, Validators.pattern('https?://.+')]],
+      description: [''],
+      publisher: ['', [Validators.required]],
+      developer: ['', [Validators.required]],
+      esrb: ['', [Validators.required]],
+      license: ['', [Validators.required]]
+  })
+
+  @Output() submit: EventEmitter<IRawVideoGameSubmission> = new EventEmitter();
+
+  constructor(private fb: FormBuilder) {
+
   }
 
-
   onSubmit() {
+    const videoGameForm: VideoGameForm = new VideoGameForm({
+      publishers: this.publishers,
+      developers: this.developers,
+      licences: this.licenses,
+      esrbs: this.esrbs
+    });
 
+    const videoGameSubmission: IRawVideoGameSubmission = {
+      title_in: this.reviewFormGroup.get('title')?.value,
+      game_picture_url_in: this.reviewFormGroup.get('imageUrl')?.value,
+      description_in: this.reviewFormGroup.get('description')?.value || '',
+      id_publisher: this.reviewFormGroup.get('publisher')?.value.id_company,
+      id_developer: this.reviewFormGroup.get('developer')?.value.id_company,
+      id_esrb_in: this.reviewFormGroup.get('esrb')?.value.id_esrb,
+      id_license_in: this.reviewFormGroup.get('license')?.value.id_license
+    };
+
+    if (!videoGameSubmission.title_in
+      || !videoGameSubmission.game_picture_url_in
+      || !videoGameSubmission.id_publisher
+      || !videoGameSubmission.id_developer
+      || !videoGameSubmission.id_esrb_in
+      || !videoGameSubmission.id_license_in
+    ) {
+      return;
+    }
+
+    console.log(videoGameSubmission);
+
+    this.submit.emit(videoGameSubmission);
+    this.reviewFormGroup.reset();
   }
 }
