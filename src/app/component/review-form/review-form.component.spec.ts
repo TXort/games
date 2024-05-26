e328c533a6051aab2b62daf93fad831a1ae0b774
TXort <46675337+TXort@users.noreply.github.com>
2024-05-26 23:45:34 +0200

    Add unit testing for forms

--- src/app/component/review-form/review-form.component.spec.ts
+++ src/app/component/review-form/review-form.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ReviewFormComponent } from './review-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {ReviewFormComponent} from "./review-form.component";
 
 describe('ReviewFormComponent', () => {
   let component: ReviewFormComponent;
@@ -8,10 +8,10 @@ describe('ReviewFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ReviewFormComponent]
+      imports: [ ReactiveFormsModule ]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(ReviewFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,24 @@ describe('ReviewFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be invalid when a required field is empty', () => {
+    component.reviewFormGroup.controls['comment'].setValue('');
+    component.reviewFormGroup.controls['title'].setValue('');
+    expect(component.reviewFormGroup.valid).toBeFalsy();
+  });
+
+  it('should be invalid there is a forbidden word', () => {
+    component.reviewFormGroup.controls['comment'].setValue('Ovo je glupa igrica!');
+    component.reviewFormGroup.controls['title'].setValue('');
+    expect(component.reviewFormGroup.valid).toBeFalsy();
+  });
+
+  it('should be valid when all fields are filled correctly', () => {
+    component.reviewFormGroup.controls['comment'].setValue('Test comment');
+    component.reviewFormGroup.controls['title'].setValue('Test title');
+    component.reviewFormGroup.controls['rating'].setValue('5');
+    expect(component.reviewFormGroup.valid).toBeTruthy();
   });
 });
--- src/app/component/videogame-form/videogame-form.component.spec.ts
+++ src/app/component/videogame-form/videogame-form.component.spec.ts
@@ -1,23 +1,47 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormBuilder } from '@angular/forms';
 import { VideogameFormComponent } from './videogame-form.component';
 
 describe('VideogameFormComponent', () => {
   let component: VideogameFormComponent;
-  let fixture: ComponentFixture<VideogameFormComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [VideogameFormComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(VideogameFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  beforeEach(() => {
+    component = new VideogameFormComponent(new FormBuilder());
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be invalid when a required field is empty', () => {
+    component.reviewFormGroup.controls['title'].setValue('');
+    component.reviewFormGroup.controls['imageUrl'].setValue('');
+    component.reviewFormGroup.controls['description'].setValue('');
+    component.reviewFormGroup.controls['publisher'].setValue('');
+    component.reviewFormGroup.controls['developer'].setValue('');
+    component.reviewFormGroup.controls['esrb'].setValue('');
+    component.reviewFormGroup.controls['license'].setValue('');
+    expect(component.reviewFormGroup.valid).toBeFalsy();
+  });
+
+  it('should be invalid when a image is not provided', () => {
+    component.reviewFormGroup.controls['title'].setValue('Half-Life');
+    component.reviewFormGroup.controls['imageUrl'].setValue('This is not an image');
+    component.reviewFormGroup.controls['description'].setValue('Bla bla');
+    component.reviewFormGroup.controls['publisher'].setValue('1');
+    component.reviewFormGroup.controls['developer'].setValue('1');
+    component.reviewFormGroup.controls['esrb'].setValue('1');
+    component.reviewFormGroup.controls['license'].setValue('1');
+    expect(component.reviewFormGroup.valid).toBeFalsy();
+  });
+
+  it('should be invalid when a image is not provided', () => {
+    component.reviewFormGroup.controls['title'].setValue('Half-Life');
+    component.reviewFormGroup.controls['imageUrl'].setValue('https://cdn.mobygames.com/455b871a-abb9-11ed-aecf-02420a000198.webp');
+    component.reviewFormGroup.controls['description'].setValue('Bla bla');
+    component.reviewFormGroup.controls['publisher'].setValue('1');
+    component.reviewFormGroup.controls['developer'].setValue('1');
+    component.reviewFormGroup.controls['esrb'].setValue('1');
+    component.reviewFormGroup.controls['license'].setValue('1');
+    expect(component.reviewFormGroup.valid).toBeTruthy();
   });
 });
